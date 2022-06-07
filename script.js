0a1fa36bba7b40bf45afc33d8c4cc440585662c9
Elizaveta Romanenko <likashan@mail.ru>
2022-06-07 23:21:09 +0700

    33.4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-let num1 = prompt('Введите сторону квадрата');
+let num1 = prompt('Введите ширину');
+let num2 = prompt('Введите длину');
 
-alert(Number(num1) * Number(num1));
+alert(Number(num1) * Number(num2));
