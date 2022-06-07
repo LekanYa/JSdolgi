3c4999f817e80a5164534b5138a2a99b91a9157c
Elizaveta Romanenko <likashan@mail.ru>
2022-06-08 00:30:03 +0700

    38.3

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let arr = ['a', 'b', 'c'];
-alert (arr[0], arr[1], arr[2]);
-console.log (arr[0], arr[1], arr[2]);
+let arr = ['a', 'b', 'c', 'd'];
+alert (arr[0] + '+' + arr[1] + '+' + arr[2] + '+' arr[3]);
+
