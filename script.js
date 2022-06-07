1aca47a09df881f4f9a4535f40830ac13c72605b
Elizaveta Romanenko <likashan@mail.ru>
2022-06-07 23:19:01 +0700

    33.2

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-let age = prompt('Ваш возраст?');
-alert('Ваш возраст: ' + age);
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+
+alert(Number(num1) + Number(num2));
