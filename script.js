fc567a3ca1e404f4764ba5e2c66e2b8ee7828f26
Elizaveta Romanenko <likashan@mail.ru>
2022-06-07 23:33:21 +0700

    34.4

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let str = 'text';
-document.write('<b>' + str + '</b>'); 
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
