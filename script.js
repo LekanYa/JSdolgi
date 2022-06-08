4082ceb5c170c478240e4e65ab605703cbf9ce4b
Elizaveta Romanenko <likashan@mail.ru>
2022-06-08 22:50:11 +0700

    57.1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Я обязательно закрою все долги');
+let month = 4;
+
+if (month == 1 || month == 2 || month == 12) {
+    alert ('зима');
+}
+if (month >= 3 && month <=5){
+    alert ('весна');
+}
+if (month >= 6 && month <=8){
+    alert ('лето');
+}
+if (month >= 9 && month <=11){
+    alert('осень');
+}
