b9f4700b02fafb531a3e179583e086659b4d81db
Elizaveta Romanenko <likashan@mail.ru>
2022-06-08 22:37:06 +0700

    56.3

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let arr = [1, 2, 3, 4, 5, 6];
+let num = 12345;
+let last = String(num)[4];
 
-if (arr.length >= 3) {
-	console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr [4] + arr[5]);
+if (last == 0) {
+	console.log('верно');
+} else {
+	console.log('неверно');
 }
