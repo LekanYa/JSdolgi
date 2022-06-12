866e7db2adae3dd97ecd55ee0d0d758a5d653ec5
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 20:36:44 +0700

    86.7

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let obj = {1: 125, 2: 225, 3: 128, 4: 356, 5: 145, 6: 281, 7: 452,};
-let arr = [];
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let obj = {};
+let i = 1;
 
-for (let key in obj) {
- let str = String(obj[key]);
-if (str[0] == 1 || str[0] == 2) {
-    arr.push(obj[key]);
-}
+for (let elem of arr) {
+obj[i] = elem;
+i++;
 }
 
-console.log(arr);
+console.log(obj);
