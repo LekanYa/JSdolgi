6a40063b34a9a367c1c23cbca74d156d646f75e3
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 14:33:47 +0700

    57.5

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let n = 123;
-let sum = 0;
+let n = 123456;
+let sum1 = 0;
+let sum2 = 0;
 
-while (n > 0) {
-    sum += n % 10
-    n = Math.floor(n/10)
+let n2 = n % 1000;
+let n1 = Math.floor(n/1000);
+
+while (n1 > 0) {
+    sum1 += n1 % 10;
+    n1 = Math.floor(n1/10);
 }
 
-alert(sum)
+while (n2 > 0) {
+    sum2 += n2 % 10;
+    n2 = Math.floor(n2/10);
+}
+
+if (sum1 == sum2){
+    alert('да');
+} else {
+    alert('нет');
+}
