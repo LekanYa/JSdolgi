b895aa06f1c23f476cee6287e8396f495e319ecb
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 19:43:52 +0700

    84.2

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 let arr = [];
 
-arr.push(1);
-arr.push(2);
-arr.push(3);
-arr.push(4);
-arr.push(5);
-arr.push(6);
-arr.push(7);
-arr.push(8);
-arr.push(9);
-arr.push(10);
+for (let i = 1; i <= 10; i++) {
+	arr.push(i);
+}
+
+console.log(arr);
