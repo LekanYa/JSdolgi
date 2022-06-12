eac17769dacf40ac25ca10d1839035bc6b8482c7
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 19:42:08 +0700

    84.1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert('Я обязательно закрою все долги');
+let arr = [];
+
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(4);
+arr.push(5);
+arr.push(6);
+arr.push(7);
+arr.push(8);
+arr.push(9);
+arr.push(10);
