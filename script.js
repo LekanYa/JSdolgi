4dfb8a7824ccb11342cc773ee845d51d3ae55865
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 20:25:22 +0700

    86.1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('Я обязательно закрою все долги');
+let obj = {
+    employee1: 100,
+    employee2: 200,
+    employee3: 300,
+    employee4: 400,
+    employee5: 500,
+    employee6: 600,
+    employee7: 700,
+    };
+    
+    for (let key in obj) {
+    obj[key] += (obj[key] * 10) / 100 ;
+    }
+    
+    console.log(obj);
