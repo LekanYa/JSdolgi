10f964adc072d5685927e43bd521107f652a2434
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 16:46:12 +0700

    72.2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let arr = [1, 2, 3, 4, 5, 0, 4, 1];
+let arr = [1, 2, 3, 4, 5, 0, -4, 1];
 let sum = 0;
 
 for (let elem of arr) {
     sum += elem;
-	if (elem == 0) {
+	if (elem > 0) {
 		break; 
 	}
 }
