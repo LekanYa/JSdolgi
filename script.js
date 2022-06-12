67f704876d8f45af1a451d05dbe0aff0c9da38e8
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 15:58:29 +0700

    67.10

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 1, 3, 4, 4];
 
-for (let i = arr.length - 1; i >= 0; i--){
-	console.log(arr[i]);
+for (let i = 0; i < arr.length; i++){
+	if (arr[i] == i){
+		console.log(arr[i])
+	}
 }
