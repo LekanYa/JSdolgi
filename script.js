5be95f607c7815e518f67131cf9387b0034cac67
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 15:55:20 +0700

    67.6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let arr = [1, 2, 3, 4, 5];
-let result = arr [0]**2;
+let arr = [2, 5, 9, 15, 1, 4];
 		
-for (let i = 1; i < arr.length; i++) { 
-	result += arr[i] ** 2;
+for (let i = 0; i < arr.length; i++) { 
+	if ((arr[i] > 3) && (arr[i] < 10)){
+		console.log(arr[i]);
+	}
 }
-console.log(result);
