74acee84277e21a01f16efb177cedd3de6c27104
Elizaveta Romanenko <likashan@mail.ru>
2022-06-13 00:41:53 +0700

    95.11

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let arr = [1, 2, 3, 4, 5];
-let sum = 0;
+let n = 5;
+let fact = 1;
 
-for(var i = 0; i < arr.length; i++) {
-sum += arr[i];
+for (let i = 1; i <= 5; i++){
+    fact *= i;
 }
 
-let average = sum / arr.length;
-alert(average);
+alert(fact);
