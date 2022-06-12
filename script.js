31a32044550544f1fbc2e04bbf4626a86a0a0be2
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 20:59:29 +0700

    88.2

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let one = 0;
 let two = 1;
+let three = 2;
 
 for (let i = 1; i <= 10; i++) {
-	let current = one + two;
-	
-	one = two;
-	two = current;
-	
-	console.log(current);
+let current = one + two + three;
+
+one = two;
+two = three;
+three = current;
+
+console.log(current);
 }
