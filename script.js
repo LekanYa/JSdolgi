14ffcc785fd682ddade96e6cf512ee1cff8c0582
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 16:07:42 +0700

    68.4

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 
 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-for (let mon of arr){
-	console.log(mon);
+let month = 10;
+for (let i = 0; i < arr.length; i++){
+	if (i == (month - 1)){
+		document.write('<i>', arr[i], '</i>', ' ');
+	}
+	else{
+		document.write(arr[i], ' ');
+	}
 }
