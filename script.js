1980a725a267c1126dfe1c8e65ee2384aa373a47
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 16:01:14 +0700

    67.13

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let arr = [1, 1, 3, 4, 4];
+let arr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 
 for (let i = 0; i < arr.length; i++){
-	document.write('<p>', arr[i], '</p>');
+	if ((arr[i] == 'Вс') || (arr[i] == 'Сб')) {
+		document.write('<b>', arr[i], '<b>', ' ')
+	}
+	else{
+		document.write(arr[i], ' ')
+	} 		
 }
