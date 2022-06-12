82bd103572a45a72a5d603b41d9fc4c180624e9f
Elizaveta Romanenko <likashan@mail.ru>
2022-06-13 00:32:02 +0700

    95.7

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let arr = [1,3,2,89,42,5,7,-34,36,0,-6,4,-9,7];
 
 for(let i = 0; i < arr.length; i++) {
-if(arr[i] > 0 && arr[i] < 10) alert(arr[i]);
+if(arr[i] == 5) {
+alert('Есть');
+break;
+}
 }
