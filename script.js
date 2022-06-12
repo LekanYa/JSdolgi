6ec5fc90cabbf4d3b67ade0238bdbb612404ee7c
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 21:44:39 +0700

    92.1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Я обязательно закрою все долги');
+let arr = [10, 20, 30, 40, 21, 32, 51];
+let sum = 0;
+
+for (let elem of arr) {
+	if (Math.floor(elem / 10) === 1 || Math.floor(elem / 10) === 2) {
+		sum += elem;
+	}
+}
+
+console.log(sum);
