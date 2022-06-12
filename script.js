7a0d941faef05494e5a658ec6d4f0896787abb0d
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 16:06:51 +0700

    68.2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-for (let letter of arr){
-	console.log(letter);
+let arr = [1, 2, 3, 4, 5];
+let result = 0;
+for (let num of arr){
+	result += num;
 }
+console.log(result);
