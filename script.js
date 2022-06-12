efb017c7fa315c4a730502e0b629c9bdd0a3ba87
Elizaveta Romanenko <likashan@mail.ru>
2022-06-13 00:35:18 +0700

    95.8

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let arr = [1,3,2,89,42,5,7,-34,36,0,-6,4,-9,7];
 
-for(let i = 0; i < arr.length; i++) {
-if(arr[i] == 5) {
-alert('Есть');
-break;
+var sum = 0;
+for(var i = 0; i < arr.length; i++) {
+sum += arr[i];
 }
-}
+alert(sum);
