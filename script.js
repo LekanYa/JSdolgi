ae664238be7670be8a06fe8ae2a6905131e7a85e
Elizaveta Romanenko <likashan@mail.ru>
2022-06-12 20:15:55 +0700

    85.5

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let obj = {'a': 12, 'b': 21, 'c': 13, 'd': 23, 'e': 17}; 
+let obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'};
 let result = {};
 
 for (let key in obj) {
-	if (obj[key] >= 10 || obj[key] <= 20) {
-		result[key] = obj[key];
-	}
+if (key % 2 != 0) {
+result[key] = obj[key];
 }
-
-console.log(result);
+}
